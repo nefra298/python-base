44f81df0b2b0152f52ac6a0a5ac7027274b211ff
nefra <nefra298@gmail.com>
2019-09-23 18:45:09 +0300

    hw-3 circle area

---
+++ lesson01/hw_3.py
@@ -0,0 +1,24 @@
+import math
+
+
+def tonumeric(inp):
+    try:
+        val = int(inp)
+        return val
+    except ValueError:
+        try:
+            val = float(inp)
+            return val
+        except ValueError:
+            val = None
+            return val
+
+
+print ("Calculating the area of a circle")
+inp1 = input("Enter a radius:")
+r = tonumeric(inp1)
+if (r is not None):
+    area = math.pi * r * r
+    print("The area of a rectangle:", area)
+else:
+    print("Please use numeric values!")
