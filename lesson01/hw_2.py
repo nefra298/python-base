cc85a6dc84c8457327166c597d9d51aae1211c18
nefra <nefra298@gmail.com>
2019-09-23 18:46:47 +0300

    hw-3 circle area

--- lesson01/hw_2.py
+++ lesson01/hw_2.py
@@ -1,4 +1,4 @@
-def ToNumeric(inp):
+def tonumeric(inp):
     try:
         val = int(inp)
         return val
@@ -7,20 +7,22 @@ def ToNumeric(inp):
             val = float(inp)
             return val
         except ValueError:
-            val=None
+            val = None
             return val
+
+
 print ("Calculating the area of a triangle")
-inp1=input("Enter the length of side A:")
-inp2=input("Enter the length of side B:")
-inp3=input("Enter the length of side C:")
-a=ToNumeric(inp1)
-b=ToNumeric(inp2)
-c=ToNumeric(inp3)
+inp1 = input("Enter the length of side A:")
+inp2 = input("Enter the length of side B:")
+inp3 = input("Enter the length of side C:")
+a = tonumeric(inp1)
+b = tonumeric(inp2)
+c = tonumeric(inp3)
 if (a is not None) and (b is not None) and (c is not None):
-    if (a<b+c) and (b<a+c) and (c<a+b):
-        p=(a+b+c)/2
-        area=(p*(p-a)*(p-b)*(p-c))**0.5
-        print("The area of a triangle:",area)
+    if (a < b + c) and (b < a + c) and (c < a + b):
+        p = (a + b + c) / 2
+        area = (p * (p - a) * (p - b) * (p - c)) ** 0.5
+        print("The area of a triangle:", area)
     else:
         print("No such triangle!")
 else:
