93f18713d3dac397730b3136e4c51db0dc102b70
nefra <nefra298@gmail.com>
2019-09-23 18:27:34 +0300

    hw-2 triangle area

---
+++ lesson01/hw_2.py
@@ -0,0 +1,27 @@
+def ToNumeric(inp):
+    try:
+        val = int(inp)
+        return val
+    except ValueError:
+        try:
+            val = float(inp)
+            return val
+        except ValueError:
+            val=None
+            return val
+print ("Calculating the area of a triangle")
+inp1=input("Enter the length of side A:")
+inp2=input("Enter the length of side B:")
+inp3=input("Enter the length of side C:")
+a=ToNumeric(inp1)
+b=ToNumeric(inp2)
+c=ToNumeric(inp3)
+if (a is not None) and (b is not None) and (c is not None):
+    if (a<b+c) and (b<a+c) and (c<a+b):
+        p=(a+b+c)/2
+        area=(p*(p-a)*(p-b)*(p-c))**0.5
+        print("The area of a triangle:",area)
+    else:
+        print("No such triangle!")
+else:
+    print("Please use numeric values!")
