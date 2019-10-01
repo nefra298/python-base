ee861e6cda474e8926dc8a1c2f6c88194f7450a7
nefra <nefra298@gmail.com>
2019-10-01 13:55:39 +0300

    hw_3_1

---
+++ lesson03/hw_3_1.py
@@ -0,0 +1,40 @@
+def tonumeric(inp):
+    try:
+        val = int(inp)
+        return val
+    except ValueError:
+        try:
+            val = float(inp)
+            return val
+        except ValueError:
+            val = None
+            return val
+
+
+def input_check_int(inp, msg=False):
+    x = tonumeric(inp)
+    if type(x) is int:
+        return True
+    else:
+        if msg:
+            print("Вы ввели не целое число")
+        return False
+
+
+def calc(inp):
+    n = 0
+    for i in inp:
+        if i.isdigit():
+            n = n + 1
+    return n
+
+
+if __name__ == '__main__':
+    while True:
+        a = input("Введите целое число или q для выхода:")
+        if (a == "q"):
+            exit()
+        elif input_check_int(a, True):
+            print("Количество цифер в числе:", calc(a))
+        else:
+            continue
