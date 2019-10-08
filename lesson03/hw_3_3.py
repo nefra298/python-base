09b146ac44ce27463f4f4425708910beb5899fbe
nefra <nefra298@gmail.com>
2019-10-08 15:31:14 +0300

    hw_3_3 add input check

--- lesson03/hw_3_3.py
+++ lesson03/hw_3_3.py
@@ -1,17 +1,22 @@
-def triangle(a,b,c):
+def triangle(a, b, c):
     if (a is not None) and (b is not None) and (c is not None):
         if (a < b + c) and (b < a + c) and (c < a + b):
             p = (a + b + c) / 2
-            area = round((p * (p - a) * (p - b) * (p - c)) ** 0.51,2)
+            area = round((p * (p - a) * (p - b) * (p - c)) ** 0.51, 2)
             print("Площать треугольника:", area)
         else:
             print("Треугольник не существует!")
+
+
 def circle(r):
     import math
-    area = round(math.pi * r * r,2)
+    area = round(math.pi * r * r, 2)
     print("Площать круга:", area)
-def rect(a,b):
-    print("Площать прямоугольника:", round(a * b,2))
+
+
+def rect(a, b):
+    print("Площать прямоугольника:", round(a * b, 2))
+
 
 def tonumeric(inp):
     try:
@@ -25,6 +30,7 @@ def tonumeric(inp):
             val = None
             return val
 
+
 if __name__ == '__main__':
     while True:
         print(
@@ -34,7 +40,7 @@ if __name__ == '__main__':
             3. Расчет площади прямоугольника
             4. Выход
             """)
-        menu_item=input("Действие(1-4):")
+        menu_item = input("Действие(1-4):")
         if (menu_item == "4"):
             exit()
         elif menu_item == "1":
@@ -43,7 +49,7 @@ if __name__ == '__main__':
             b = input("Введите длинну стороны B:")
             c = input("Введите длинну стороны C:")
             if (tonumeric(a) is not None) and (tonumeric(b) is not None) and (tonumeric(c) is not None):
-                triangle(float(a),float(b),float (c))
+                triangle(float(a), float(b), float(c))
                 input("Нажмите любую кнопку что бы продолжить")
             else:
                 print("Введены не корректные значения")
